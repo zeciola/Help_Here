0215f21fec81a6e2089874069ad453c8f82d1038
ZeCiola <jr.ciola.bricio@gmail.com>
2017-02-08 10:59:25 -0200

    simple registration create
    create a file Cadastro

--- HelpHererJavaWeb/src/java/Model/Cadastro.java
+++ HelpHererJavaWeb/src/java/Model/Cadastro.java
@@ -6,5 +6,86 @@ public class Cadastro {
     private String celular;
     private String endereco;
     private String estado;
-    private String 
+    private String cep;
+    private String RG;
+    private String cpf;
+    private String sexo;
+    
+    //Gets
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+    
+    //Sets
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    
+    
 }
