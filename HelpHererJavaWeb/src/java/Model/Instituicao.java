6823b39a29d7631dbc79bfeee7326cd170fb23e0
dslz13 <diegoselzzo13@hotmail.com>
2017-02-15 09:59:45 -0200

    add class in Model
    Evento, Instituicao

---
+++ HelpHererJavaWeb/src/java/Model/Instituicao.java
@@ -0,0 +1,53 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Instituicao {
+
+    private String nome;
+    private String razao;
+    private String cnpj;
+    private ArrayList<Endereco> endereco = new ArrayList();
+    private String modalidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRazao() {
+        return razao;
+    }
+
+    public void setRazao(String razao) {
+        this.razao = razao;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public ArrayList<Endereco> getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(ArrayList<Endereco> endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getModalidade() {
+        return modalidade;
+    }
+
+    public void setModalidade(String modalidade) {
+        this.modalidade = modalidade;
+    }
+    
+}
