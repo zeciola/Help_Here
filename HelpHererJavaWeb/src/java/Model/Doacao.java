069f11fe3af08a7e51757e6597d8b63574fed486
LucasPuglia <lucas_puglia@hotmail.com>
2017-02-15 10:58:13 -0200

    atributos e metodos

--- HelpHererJavaWeb/src/java/Model/Doacao.java
+++ HelpHererJavaWeb/src/java/Model/Doacao.java
@@ -5,10 +5,77 @@
  */
 package Model;
 
+import java.util.Date;
+
 /**
  *
  * @author 11141100670
  */
 public class Doacao {
+    private Date dataInicio;
+    private Date dataFim;
+    private Date dataCad;
+    private String nome;
+    private String descricao;
+    private Pessoa responsavel;
+    private Endereco endereco;
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(Date dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public Date getDataCad() {
+        return dataCad;
+    }
+
+    public void setDataCad(Date dataCad) {
+        this.dataCad = dataCad;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Pessoa getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(Pessoa responsavel) {
+        this.responsavel = responsavel;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+    
+    
     
 }
