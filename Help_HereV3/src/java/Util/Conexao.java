be83392579fc745a06a295ee2c46332a422e9cd3
LucasPuglia <lucas_puglia@hotmail.com>
2017-04-12 00:29:43 -0300

    .

--- Help_HereV3/src/java/Util/Conexao.java
+++ Help_HereV3/src/java/Util/Conexao.java
@@ -18,7 +18,7 @@ public class Conexao {
     //login do banco
     static String login = "postgres";
     //senha do banco
-    static String password = "postgres";
+    static String password = "lucas";
     //driver que será utilizado
     static String driver = "org.postgresql.Driver";
 
@@ -33,7 +33,7 @@ public class Conexao {
             //driver que será utilizado
             Class.forName("org.postgresql.Driver");
             //cria um objeto de conexao com um banco especificado no caminho...
-            conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Helphere", "postgres", "postgres");
+            conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/HelpHere", "postgres", "lucas");
         } catch (ClassNotFoundException erro1) {
             JOptionPane.showMessageDialog(null, "Erro ao tentar carregar o driver: " + erro1);
         } catch (SQLException erro2) {
