d9bd4e5923038fd4b5d7a36b9a6dab0f8b20a8c2
dslz13 <diegoselzzo13@hotmail.com>
2017-06-06 09:57:41 -0300

    quartza 2

--- Help_HereV3/src/java/DAO/DAOEvento.java
+++ Help_HereV3/src/java/DAO/DAOEvento.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DAO;
 
 import Model.Endereco;
@@ -21,10 +16,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import static jdk.nashorn.internal.objects.NativeMath.log;
 
-/**
- *
- * @author 11141104689
- */
 public class DAOEvento implements iDAO {
 
     private Evento ev;
