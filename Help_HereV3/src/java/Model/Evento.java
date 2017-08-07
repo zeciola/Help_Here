d3f88f72f6e2d569de6125daa193907544ed1f57
dslz13 <diegoselzzo13@hotmail.com>
2017-08-07 10:18:46 -0300

    ok
    ok

--- Help_HereV3/src/java/Model/Evento.java
+++ Help_HereV3/src/java/Model/Evento.java
@@ -17,11 +17,24 @@ public class Evento {
     private String descricao;
     private boolean status;
     private String img;
-   
+    private ArrayList<String> galeria;
+    
+    public ArrayList<String> getGaleria() {
+        return galeria;
+    }
 
+    public void setGaleria(ArrayList<String> galeria) {
+        this.galeria = galeria;
+    }
     
-    
-    
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+       
     public int getIdEvento() {
         return idEvento;
     }
@@ -29,10 +42,6 @@ public class Evento {
     public void setIdEvento(int idEvento) {
         this.idEvento = idEvento;
     }
-
-    
-    
-    
     
     public boolean getStatus() {
         return status;
@@ -41,8 +50,6 @@ public class Evento {
     public void setStatus(boolean status) {
         this.status = status;
     }
-
-    
   
     public String getTipoEvento() {
         return tipoEvento;
@@ -51,9 +58,6 @@ public class Evento {
     public void setTipoEvento(String tipoEvento) {
         this.tipoEvento = tipoEvento;
     }
-
-    
-    
 
     //Gets
     public Date getDataInicio() {
@@ -64,7 +68,6 @@ public class Evento {
         return dataFim;
     }
 
-
     public String getNome() {
         return nome;
     }
@@ -72,9 +75,6 @@ public class Evento {
     public String getDescricao() {
         return descricao;
     }
-
-
-
 
     //Sets
     public void setDataInicio(Date dataInicio) {
@@ -85,7 +85,6 @@ public class Evento {
         this.dataFim = dataFim;
     }
 
-
     public void setNome(String nome) {
         this.nome = nome;
     }
@@ -93,8 +92,4 @@ public class Evento {
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
-
-
-
-
 }
