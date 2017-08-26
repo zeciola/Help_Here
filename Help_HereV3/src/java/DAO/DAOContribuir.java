63b9659a6449b2f6368a343e1792701a2f849096
dslz13 <diegoselzzo13@hotmail.com>
2017-08-26 16:18:06 -0300

    doação ok
    falta jogar dados da pessoa no boleto pdf

--- Help_HereV3/src/java/DAO/DAOContribuir.java
+++ Help_HereV3/src/java/DAO/DAOContribuir.java
@@ -1,13 +1,41 @@
 package DAO;
 
 import Model.Contribuicao;
-import Model.Usuario;
-import Model.Evento;
+
+import Util.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DAOContribuir {
-    
-    
-    public void DoarValor(Contribuicao c){
-        
+
+    public void DoarValor(Contribuicao c) {
+        Connection conexao = Conexao.getConexao();
+        try {
+            conexao.setAutoCommit(false);
+            PreparedStatement pstmt = conexao.prepareStatement("insert into valoresdoados (Valor, dataDoado, idCampanha, IDPessoa) values(?, CURRENT_DATE, ?, ?)");
+            pstmt.setDouble(1, c.getValor());
+            pstmt.setInt(2, c.getEv().getIdEvento());
+            pstmt.setInt(3, c.getUser().getId());
+            pstmt.execute();
+            conexao.commit();
+
+        } catch (SQLException e) {
+            try {
+                conexao.rollback();
+            } catch (SQLException ex) {
+                Logger.getLogger(DAOPessoa.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } finally {
+            if (conexao != null) {
+                try {
+                    conexao.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(DAOPessoa.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
     }
 }
